f71e76e24f498d45d22fad98dfb306ae46f74856
PaunikarSaloni <93531236+PaunikarSaloni@users.noreply.github.com>
2022-02-04 12:27:03 +0530

    Add files via upload

---
+++ BasicPython_ques.py
@@ -0,0 +1,123 @@
+#1Arithmetic Programming for addition
+a = 5
+b = 10
+sum = a+b
+print(sum)
+
+#2Arithmetic Programming for subtraction
+a = 20
+b = 12
+sub = a - b
+print(sub)
+
+#3Arithmetic Programming for multiplication
+a = 25
+b = 30
+mul = a*b
+print(mul)
+
+#4Arithmetic Programming for division
+a = 50
+b = 10
+div = a/b
+print(div)
+
+#5Program to calculate Area of Triangle
+b=55
+h=10
+triarea=0.5*b*h
+print(triarea)
+
+#6Program to calculate Area of rectangle
+l = 5
+b = 12
+rectarea = l*b
+print(rectarea)
+
+#7Program to calculate Area of square
+a = 5
+sqarea = a*a
+print(sqarea)
+
+#8Program to calculate Area of circle
+r = 2
+pi = 3.14
+areaofcircle=(pi*r*r)
+print(areaofcircle)
+
+#9Program to calculate volume of cuboid
+l = 2
+w = 3
+h = 5
+cuboidvolume=l*w*h
+print(cuboidvolume)
+
+#10Program to calculate volume of cube
+a = 6
+cubeVol = a*a*a
+print(cubeVol)
+
+#11Program to calculate volume of cone
+r = 5
+l = 11
+pi = 3.14
+coneVolume = (1/3)*pi*r*r*l
+print(coneVolume)
+
+#12Program to calculate volume of cylinder
+r = 5
+h = 7
+pi = 3.14
+cylinderarea = pi*r*r*h
+print(cylinderarea)
+
+#13Program to Swap two variables
+a = 10
+b = 20
+temp = a
+a = b
+b = temp
+print(a,b)
+#OR
+a = 50
+b = 80
+a = a+b
+b=a-b
+a=a-b
+print(a,b)
+
+#14Program to Reverse a three digit number
+a = 123
+rem1=a%10
+a=a//10
+rem2=a%10
+a=a//10
+rem3=a
+rev=rem1*100+rem2*10+rem3*1
+print(rev)
+
+a = 123456
+a=str(a)
+print(int(a[::-1]))
+
+#15Program to convert kilometer to meter
+km = 5
+meter = km*1000
+print(meter)
+
+#16Program to convert Celsius to Fahrenheit
+celsius = 40
+fahrenheit = (celsius * 1.8) + 32
+print(fahrenheit)
+
+#17Program to calculate (a+b)^2 and (a-b)^2
+a = 2
+b = 3
+formula = (a*a +2*(a*b)+b*b)
+print(formula)
+
+a = 2
+b = 3
+formula = (a*a+2*(a*b)-b*b)
+print(formula)
+
